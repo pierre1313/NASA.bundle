9ba5715408bdb698e0d86a9d2d723a88c002abe3
Pierre <p.dellanave@gmail.com>
2011-03-10 23:55:44 -0800

    code refresh - conversion to v2 - updated broken links - add HD Live stream

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,7 +1,5 @@
 import re,string
 import random
-from PMS import *
-
 
 ####################################################################################################
 
@@ -31,13 +29,17 @@ CACHE_NASATV            = CACHE_RSS
 CACHE_RSS_INDEX         = 72000
 CACHE_PHOTO_METADATA    = 691200
 
+ART = "art-default.jpg"
+ICON = "icon-default.png"
+SEARCH = "icon-search.png"
+
 ####################################################################################################
 
 def Start():
 
-  Plugin.AddPrefixHandler(NASA_VIDEO_PREFIX, MainMenuVideo, L('nasa'), "icon-default.png", "art-default.jpg")
-  Plugin.AddPrefixHandler(NASA_AUDIO_PREFIX, MainMenuAudio, L('nasa'), "icon-default.png", "art-default.jpg")
-  Plugin.AddPrefixHandler(NASA_PHOTO_PREFIX, MainMenuPhoto, L('nasa'), "icon-default.png", "art-default.jpg")
+  Plugin.AddPrefixHandler(NASA_VIDEO_PREFIX, MainMenuVideo, L('nasa'), ICON, ART)
+  Plugin.AddPrefixHandler(NASA_AUDIO_PREFIX, MainMenuAudio, L('nasa'), ICON, ART)
+  Plugin.AddPrefixHandler(NASA_PHOTO_PREFIX, MainMenuPhoto, L('nasa'), ICON, ART)
 
   Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
   Plugin.AddViewGroup("Details", viewMode="InfoList", mediaType="items")
@@ -45,10 +47,13 @@ def Start():
 
   MediaContainer.title1 = L("nasa")
   MediaContainer.content = 'Items'
-  MediaContainer.art = R('art-default.jpg')
+  MediaContainer.art = R(ART)
   MediaContainer.viewGroup = 'List'
-  DirectoryItem.thumb = R('icon-default.png')
-
+  DirectoryItem.thumb = R(ICON)
+  VideoItem.thumb = R(ICON)
+  WebVideoItem.thumb = R(ICON)
+  WindowsMediaVideoItem.thumb = R(ICON)
+  
 def UpdateCache():
 
   HTTP.Request(RSS_INDEX, cacheTime=CACHE_RSS_INDEX)
@@ -56,21 +61,21 @@ def UpdateCache():
 def MainMenuVideo():
 
   dir = MediaContainer()
-  dir.Append(Function(DirectoryItem(FeaturedVideoMenu, title=L('Featured'), thumb=R('icon-default.png'))))
-  dir.Append(Function(DirectoryItem(PodcastChooser, title=L('podcasts'), thumb=R('icon-default.png')), mediaType='video'))
-  dir.Append(Function(DirectoryItem(NASATV, title=L('nasatv'), thumb=R('icon-default.png'))))
-  dir.Append(Function(DirectoryItem(HDContent, title=L('HD'), thumb=R('icon-default.png'))))
-  dir.Append(Function(SearchDirectoryItem(SearchContent, title=L('search'),prompt=L('search'), thumb=R('icon-default.png'))))
+  dir.Append(Function(DirectoryItem(FeaturedVideoMenu, title=L('Featured'))))
+  dir.Append(Function(DirectoryItem(PodcastChooser, title=L('podcasts')), mediaType='video'))
+  dir.Append(Function(DirectoryItem(NASATV, title=L('nasatv'))))
+  dir.Append(Function(DirectoryItem(HDContent, title=L('HD'))))
+  dir.Append(Function(SearchDirectoryItem(SearchContent, title=L('search'),prompt=L('search'), thumb=R(SEARCH))))
 
   return dir
   
 def FeaturedVideoMenu(sender):
 
   dir = MediaContainer()
-  dir.Append(Function(DirectoryItem(FeaturedContent, title=L('mostrecent'), thumb=R('icon-default.png')), feature='mostrecent'))
-  dir.Append(Function(DirectoryItem(FeaturedContent, title=L('mostwatched'), thumb=R('icon-default.png')), feature='popular'))
-  dir.Append(Function(DirectoryItem(FeaturedContent, title=L('toprated'), thumb=R('icon-default.png')), feature='top_rated'))
-  dir.Append(Function(DirectoryItem(Archives, title=L('archives'), thumb=R('icon-default.png'))))
+  dir.Append(Function(DirectoryItem(FeaturedContent, title=L('mostrecent')), feature='mostrecent'))
+  dir.Append(Function(DirectoryItem(FeaturedContent, title=L('mostwatched')), feature='popular'))
+  dir.Append(Function(DirectoryItem(FeaturedContent, title=L('toprated')), feature='top_rated'))
+  dir.Append(Function(DirectoryItem(Archives, title=L('archives'))))
 
   return dir
   
@@ -85,8 +90,7 @@ def HDContent(sender, page = None, pagenum = 0):
 
       page = 'http://www.nasa.gov/multimedia/hd/HDGalleryCollection_archive_%s.html'%pagenum
       
-      
-  content = XML.ElementFromURL(page,isHTML=True)
+  content = HTML.ElementFromURL(page)
   for c in content.xpath("//div[@id='hdgallery']"):
     url = c.xpath("li//a")[0].get("href")
     title = c.xpath("li//h3")[0].text
@@ -107,16 +111,12 @@ def HDContent(sender, page = None, pagenum = 0):
   return dir 
   
 def ParseJSON(sender = None, url= None, pagenum = 0, total_pages = 5, videos_per_page = 15):
-
   dir = MediaContainer(replaceParent = (pagenum>0))
 
   if (pagenum >0):
     dir.Append(Function(DirectoryItem(ParseJSON,title='Previous Page'),url = url,pagenum = pagenum-1))
 
-  #url = http://cdn-api.vmixcore.com/apis/media.php?action=getMediaList&class_id=1&alltime=1&order_method=DESC&get_count=1&order=date_published_start&export=JSONP&limit%s&start=%s&&metadata=1&external_genre_ids=131&atoken=cf15596810c05b64c422e071473549f4
-  #jsondata = JSON.ObjectFromURL('http://cdn-api.vmixcore.com/apis/media.php?action=searchMedia&export=JSONP&atoken=cf15596810c05b64c422e071473549f4&fields=title,description&limit=%s&start=%s&query=%s'%((total_pages*videos_per_page),(pagenum*videos_per_page),query))
   jsondata = JSON.ObjectFromURL(url%((total_pages*videos_per_page),(pagenum*videos_per_page)))
-  #Log(jsondata)
   for item in jsondata['media']:
     Log(item['token'])
     dir.Append(Function(VideoItem(GetVideoFromToken,title=item['title'], thumb=item['thumbnail'][0]['url'],duration=int(item['duration'])*1000),token=item['token']))
@@ -130,44 +130,16 @@ def SearchContent(sender, query = None):
  
   url = 'http://cdn-api.vmixcore.com/apis/media.php?action=searchMedia&export=JSONP&atoken=cf15596810c05b64c422e071473549f4&fields=title,description&limit=%s&start=%s&query='+query
   return ParseJSON(url=url)
-  
-  #total_pages = 5
-  #videos_per_page = 15
-
-  #dir = MediaContainer(replaceParent = (pagenum>0))
-
-  #if (pagenum >0):
-  #  dir.Append(Function(DirectoryItem(SearchContent,title='Previous Page'),query = query,pagenum = pagenum-1))
-
-
-  #url = http://cdn-api.vmixcore.com/apis/media.php?action=getMediaList&class_id=1&alltime=1&order_method=DESC&get_count=1&order=date_published_start&export=JSONP&limit%s&start=%s&&metadata=1&external_genre_ids=131&atoken=cf15596810c05b64c422e071473549f4
-  #jsondata = JSON.ObjectFromURL('http://cdn-api.vmixcore.com/apis/media.php?action=searchMedia&export=JSONP&atoken=cf15596810c05b64c422e071473549f4&fields=title,description&limit=%s&start=%s&query=%s'%((total_pages*videos_per_page),(pagenum*videos_per_page),query))
-  #for item in jsondata['media']:
-  #  dir.Append(VideoItem(item['url'],title=item['title'], thumb=item['thumbnail'][0]['url'],duration=int(item['duration'])*1000))
-
-  #if (pagenum <total_pages):
-  #  dir.Append(Function(DirectoryItem(SearchContent,title='Next Page'),query = query,pagenum = pagenum+1))
-  
-  #return dir   
 
 def MainMenuAudio():
 
   return PodcastChooser(None, mediaType='audio')
 
 def PodcastChooser(sender, mediaType):
-
-  # mediaType is either 'video' or 'audio'
-
-  # Show the available shows
-
   dir = MediaContainer()
   dir.title2 = L('podcasts')
-  page = XML.ElementFromURL(RSS_INDEX, isHTML=True, cacheTime=CACHE_RSS_INDEX)
+  page = HTML.ElementFromURL(RSS_INDEX, cacheTime=CACHE_RSS_INDEX)
 
-  #if mediaType == 'video':
-  #  feeds = page.xpath("//table/tbody/tr/td/a")
-  #else:
-  #  feeds = page.xpath("//table/tbody/tr/td[position()=4]/a")
   if mediaType == 'video':
     feeds = page.xpath("//table//td[position()=2]/a")
   else:
@@ -180,16 +152,16 @@ def PodcastChooser(sender, mediaType):
     title = title.strip()
 
     # We use the image fro the podcast as the thumbnail, storing it in the dictionary after the first time
-    if not Dict.HasKey('podthumb-'+url):
+    if not ('podthumb-'+url) in Dict:
       podcast = RSS.FeedFromURL(url, cacheTime=CACHE_RSS)
       thumb = podcast.feed.image.href
-      Dict.Set('podthumb-'+url, thumb)
+      Dict['podthumb-'+url] = thumb
     else:
-      thumb = Dict.Get('podthumb-'+url)
+      thumb = Dict['podthumb-'+url]
 
     dir.Append(Function(DirectoryItem(PodcastEpisodes, title=title, thumb=thumb), title=title, url=url))
 
-  dir.Append(Function(DirectoryItem(OtherPodcastChooser, title=L('otherpodcasts'), thumb=R('icon-default.png'))))
+  dir.Append(Function(DirectoryItem(OtherPodcastChooser, title=L('otherpodcasts'))))
   return dir
 
 
@@ -200,7 +172,7 @@ def OtherPodcastChooser(sender):
   dir.title1=L('podcasts')
   dir.title2=L('otherpodcasts')
 
-  page = XML.ElementFromURL(PODCAST_INDEX, isHTML=True, cacheTime=CACHE_RSS_INDEX)
+  page = HTML.ElementFromURL(PODCAST_INDEX, cacheTime=CACHE_RSS_INDEX)
 
   feeds = page.xpath("//h2[text()='Other NASA Podcasts']/..//div[@id='ullitags']//a")
 
@@ -213,20 +185,18 @@ def OtherPodcastChooser(sender):
       continue 
     title = title[2:len(title)] # Remove first 2 characters - The bullet marks
     title = title.strip()
-    if not Dict.HasKey('podthumb-'+url):
+    if not ('podthumb-'+url) in Dict:
       podcast = RSS.FeedFromURL(url, cacheTime=CACHE_RSS)
       try:
         thumb = podcast.feed.image.href
       except:
-        thumb = R('icon-default.png')
-      Dict.Set('podthumb-'+url, thumb)
+        thumb = R(ICON)
+      Dict['podthumb-'+url] = thumb
     else:
-      thumb = Dict.Get('podthumb-'+url)
+      thumb = Dict['podthumb-'+url]
     dir.Append(Function(DirectoryItem(PodcastEpisodes, title=title, thumb=thumb), title=title, url=url))
 
   return dir
-
-
 
 def PodcastEpisodes(sender, title, url):
 
@@ -244,8 +214,10 @@ def PodcastEpisodes(sender, title, url):
     title = entry.title
     url = entry.enclosures[0].url
     summary = entry.description
-    date = Datetime.ParseDate(entry.date).strftime('%a %b %d, %Y')
-    
+    try:
+      date = Datetime.ParseDate(entry.date).strftime('%a %b %d, %Y')
+    except:
+      date = ''
     #Log(url)
   
     if (url.count('mp3') > 0) :
@@ -264,18 +236,8 @@ def GetVideoFromToken(sender,token):
       smil = Element.xpath('//play_url')[0].text
       height = Element.xpath('//height')[0].text
       width = Element.xpath('//width')[0].text
-      
-      Log(smil)
-      
-      VideoElement = XML.ElementFromURL(smil)
-      if VideoElement != None:
-        url = VideoElement.xpath('//meta')[0].get('base')
-        clip = VideoElement.xpath('//video')[0].get('src')
-        return Redirect (RTMPVideoItem(url = url,clip = clip,width = width,height = height))
-      else:
-        return None
-
-    
+      return Redirect (VideoItem(smil,width = width,height = height))
+   
 def FeaturedContent(sender, feature):
 
   dir = MediaContainer()
@@ -290,37 +252,9 @@ def FeaturedContent(sender, feature):
       duration = int(item.xpath('duration')[0].text)*1000
       dir.Append(Function(VideoItem(GetVideoFromToken, title = title, thumb=thumb,duration=duration),token=token))
     return dir
-#  elif feature == 'mostwatched':
-    # The most watched content list is supplied by a javascript file
-#    videoJS = HTTP.Request(MOST_WATCHED, cacheTime=CACHE_GALLERY)
-    # Extract the HTML from the javascript
-#    videoHTML = re.search(r'document.write\((.*)\);}', videoJS).group(1)
-#    videoXML = XML.ElementFromString(videoHTML, isHTML=True)
   else:
     url = 'http://cdn-api.vmixcore.com/apis/media.php?action=getMediaList&class_id=1&alltime=1&order_method=DESC&get_count=1&order=date_published_start&export=JSONP&limit%s&start=%s&&metadata=1&external_genre_ids=131&atoken=cf15596810c05b64c422e071473549f4'
     return ParseJSON(url=url)
-  #  videoXML = XML.ElementFromURL(VIDEO_GALLERY, isHTML=True, cacheTime=CACHE_GALLERY)
-
-  #videos = videoXML.xpath("//div[@class='myOverlayVideo bookmark right narrow_overlay']/following-sibling::div[position()=1]")
-#  videos = videoXML.xpath("//div[@class='hide' and @id='myOverlayContent']")
-#  for video in videos:
-#    title = video.xpath(".//h6")[0].text
-#    thumb = NASA_URL + video.xpath("./preceding-sibling::a/img")[0].get('src')
-#    if feature == 'mostwatched':
-#      urlJS = video.xpath(".//a[@class='bookmarkbutton']")[0].get('onclick')
-#      url = re.search (r"(http://[^\\]+)\\", urlJS).group(1)
-#    else:
-#      urlJS = video.xpath(".//a[@class='bookmarkbutton']")[0].get('href')
-#      url = re.search (r"(http://[^\|]+)\|", urlJS).group(1)
-
-#    Log(url)
-#    if (url.count('asx') > 0) :
-#      dir.Append(WindowsMediaVideoItem(url, title=title, duration=str(0), thumb=thumb))
-#    else:
-#      dir.Append(VideoItem(url, title=title, duration=str(0), thumb=thumb))
-
-
-#  return dir
 
 def Archives(sender, url=VIDEO_ARCHIVES, pageNumber=1):
 
@@ -331,7 +265,7 @@ def Archives(sender, url=VIDEO_ARCHIVES, pageNumber=1):
     dir.title1 = L('archives')
     dir.title2 = L('page') + " " + pageNumber
 
-  page = XML.ElementFromURL(url, isHTML=True, cacheTime=CACHE_ARCHIVES)
+  page = HTML.ElementFromURL(url, cacheTime=CACHE_ARCHIVES)
 
   videos = page.xpath("//div[@id='browseArchive']/ul/li")
 
@@ -356,51 +290,35 @@ def Archives(sender, url=VIDEO_ARCHIVES, pageNumber=1):
   nextPageUrl = NASA_URL + page.xpath("//a[@class='archive_forward']")[0].get('href')
   if nextPageUrl != url:
     pageNumber = pageNumber + 1
-    dir.Append(Function(DirectoryItem(Archives, title=L('nextpage'), thumb=R('icon-default.png')), url=nextPageUrl, pageNumber=pageNumber))
+    dir.Append(Function(DirectoryItem(Archives, title=L('nextpage')), url=nextPageUrl, pageNumber=pageNumber))
 
   return dir
 
 def NASATV(sender):
 
-  dir = MediaContainer()
-  dir.title2 = L('nasatv')
-
-  page = XML.ElementFromURL(NASA_TV, isHTML=True, cacheTime=CACHE_NASATV)
-
-  channels = page.xpath("//div[@class='nasa_tv_chan']")
-
-  for channel in channels:
-    title = channel.xpath(".//a")[0].text
-    urlJS = channel.xpath(".//a")[0].get('href')
-    url = re.search ("(http://[^']+)'", urlJS).group(1)
-
-    #Log(url)
-
-    if (url.count('asx') > 0) :
-      dir.Append(WindowsMediaVideoItem(url, title=title, duration=str(0), thumb=R('icon-default.png')))
-    else:
-      dir.Append(VideoItem(url, title=title, duration=str(0), thumb=R('icon-default.png')))
+  dir = MediaContainer(title2 = L('NASATV'))
+      
+  dir.Append(WindowsMediaVideoItem('http://www.nasa.gov/55644main_NASATV_Windows.asx',title='Public Channel - SD', duration=str(0)))
+  dir.Append(WebVideoItem('http://cdn1.ustream.tv/swf/4/viewer.322.swf?autoplay=true&brand=embed&cid=6540154&v3=1',title='Public Channel - HD', duration=str(0)))
+  dir.Append(WindowsMediaVideoItem('http://www.nasa.gov/145590main_Digital_Media.asx', title='Media Channel', duration=str(0)))
+  dir.Append(WindowsMediaVideoItem('http://www.nasa.gov/145588main_Digital_Edu.asx',title='Education Channel', duration=str(0)))
+  dir.Append(WindowsMediaVideoItem('http://www.nasa.gov/multimedia/isslivestream.asx',title='Space Station Views', duration=str(0)))
 
   return dir
-    
 
 def MainMenuPhoto():
 
   dir = MediaContainer()
 
-  dir.Append(Function(DirectoryItem(ImageOfTheDay, title=L('mostrecent'), thumb=R('icon-default.png')), order='mostrecent'))
-  dir.Append(Function(DirectoryItem(ImageOfTheDay, title=L('random'), thumb=R('icon-default.png')), order='random'))
+  dir.Append(Function(DirectoryItem(ImageOfTheDay, title=L('mostrecent')), order='mostrecent'))
+  dir.Append(Function(DirectoryItem(ImageOfTheDay, title=L('random')), order='random'))
 
   return dir
 
 def ImageOfTheDay(sender, order):
+  dir = MediaContainer(title2 = L(order),viewGroup = 'Pictures')
 
-
-  dir = MediaContainer()
-  dir.title2 = L(order)
-  dir.viewGroup = 'Pictures'
-
-  iotdXML = XML.ElementFromURL(NASA_IMAGE_OF_THE_DAY, isHTML=False, cacheTime = 0)
+  iotdXML = XML.ElementFromURL(NASA_IMAGE_OF_THE_DAY, cacheTime = 0)
 
   allImages = iotdXML.xpath("//ig")
   images = []
@@ -413,8 +331,6 @@ def ImageOfTheDay(sender, order):
   for image in images:
 
     thumbnailUrl = NASA_URL + image.xpath("./tn")[0].text
-    #PMS.Log(thumbnailUrl)
-
 
   # We need to pull the meta data for each image, for speed lets thread it
   @parallelize
@@ -429,9 +345,9 @@ def ImageOfTheDay(sender, order):
     
     thumbnailUrl = NASA_URL + image.xpath("./tn")[0].text
     metadataUrl = NASA_URL + image.xpath("./ap")[0].text + '.xml'
-    metadata = XML.ElementFromURL(metadataUrl, isHTML=False, cacheTime= CACHE_PHOTO_METADATA)
+    metadata = XML.ElementFromURL(metadataUrl, cacheTime= CACHE_PHOTO_METADATA)
     thumbnailUrl = NASA_URL + metadata.xpath("//thumbnail")[0].text
-    #PMS.Log(thumbnailUrl)
+
     description = StripHTML(metadata.xpath("//channel/description")[0].text, paragraphsToNewLines=True)
     title = metadata.xpath("//channel/title")[0].text
     # Try to find the largest size (other than 'original') with the right aspect ratio (that matched the thumbnail), 1600 x 1200 ideally. 
